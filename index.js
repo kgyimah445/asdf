0b32f0b6a993ee5cdf699663a8054691b33d42d7
kgyimah445 <kgyimah445@gmail.com>
2023-02-08 01:51:53 +0000

    adding getDistance

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-(function(window, _) {
-    window.myLibraryName = window.myLibraryName || {
-      numz: {},
-      phyz: {},
-    };
-  }(window, window._));
+(function (window, _) {
+  window.asdf = window.asdf || {
+    numz: {},
+    phyz: {
+      /**
+       *
+       * @param {object} obj1 - An object
+       * @param {object} obj2 - An object
+       */
+      getDistance(pointA, pointB) {
+        const distanceX = pointB.x - pointA.x,
+          distanceY = pointB.y - pointA.y,
+          distance = Math.sqrt(distanceX * distanceX - distanceY * distanceY);
+        return distance;
+      },
+    },
+  };
+})(window, window._);
+
+asdf.phyz.calcDistanc;
