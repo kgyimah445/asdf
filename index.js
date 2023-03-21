4a132d30b4d3b80827dff6d82d3e2265e89dbecc
kgyimah445 <kgyimah445@gmail.com>
2023-03-21 22:42:15 +0000

    Adding Degrees and radians

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 (function (window, _) {
   window.asdf = window.asdf || {
-    numz: {},
+    numz: {
+      
+      /**
+             * Turns degrees into radians
+             * @param {number} degrees 
+             * @returns {number} the degrees in radians
+             */
+      degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+    },
+    /**
+     * Turns radians into degrees
+     * @param {number} radians 
+     * @returns {number} the degrees in radians
+     */
+    radiansToDegrees(radians){
+        return radians * 180 / Math.PI;
+    },
+    /**
+     * Calculates the angle between two points
+     * @param {object} pointA
+     * @param {number} pointA.x - x value of pointA
+     * @param {number} pointA.y - y value of pointA
+     * @param {object} pointB
+     * @param {number} pointB.x - x value of pointB
+     * @param {number} pointB.y - y value of pointB
+     * @returns {number} Angle between the two points in degrees. Value range: -180 to 180
+     */
+    getAngleDegrees(pointA, pointB){
+        const
+            distanceX = pointB.x - pointA.x,
+            distanceY = pointB.y - pointA.y,
+            radians = Math.atan2(distanceY, distanceX),
+            degrees = radians * 180 /Math.PI;
+        return degrees;
+
+
+    }
+
+
+
+    },
     phyz: {
       /**
        *
