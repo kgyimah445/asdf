941275ae19c4c4f312b9b0a462b19bdc2971f234
kgyimah445 <kgyimah445@gmail.com>
2023-04-04 23:07:44 +0000

    redadding getdistance

--- index.js
+++ index.js
@@ -43,6 +43,22 @@
 
     },
     phyz: {
+       /**
+       *
+       * @param {object} pointA - The first point
+       * @param {object} pointB - The second point
+       * @param {number} pointB.x - Point B's X value
+       * @param {number} pointB.y - Point B's Y value
+       * @param {number} pointA.x - Point A's X value
+       * @param {number} pointA.y - Point A's Y value
+       * @returns {number} - Returns the distance between point A and point B
+       */
+       getDistance(pointA, pointB) {
+        const distanceX = pointB.x - pointA.x,
+          distanceY = pointB.y - pointA.y,
+          distance = Math.sqrt(distanceX * distanceX - distanceY * distanceY);
+        return distance;
+      },
       /**
        * Returns an Object with basic properties utilized in a 
        * 2D physics system. On top of simple physical properties,
